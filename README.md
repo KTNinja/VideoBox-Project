09638a546cbb52d18ea9a208105c3eb0ad5ba2c7
KTNinja <tiwari2000kartik@gmail.com>
2022-08-29 19:40:03 +0530

    Initial Addition

---
+++ README.md
@@ -0,0 +1,1 @@
+# Built and Deployed a Modern YouTube Clone Application in React JS with Material UI 5
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
+
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>   {/* #000 is pure black color which is for background */}
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
+export default App;
